181ed859b04c97d1f3477d3b7eac96a64e5392c2
MechaRon <mecharon369@gmail.com>
2026-03-12 19:06:21 +0800

    first-test

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import path from "path"
+
+const app = express()
+const port = 3000
+
+app.use(express.static("dist"))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("dist/index.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./styles.css"
+import { hello } from "./components/hello.js"
+
+document.getElementById("message").innerText = hello()
